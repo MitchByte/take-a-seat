ea2bd0ea67f73b907447da34219a5bb4b58017fb
Michele <fischer-michele@gmx.de>
2021-08-26 19:52:03 +0200

    added missing parts

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cookieSession = require('cookie-session');
+const path = require("path");
 
 
 const cookieSessionMiddleware = cookieSession({
